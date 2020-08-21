4f90c975b3a50c850db87e64b7b070157229bfc0
kur <rhdnfka94@gmail.com>
2020-08-21 15:51:32 +0900

    Relocate and Split functions

--- data/szmc_v0.py
+++ data/szmc_v0.py
@@ -17,7 +17,7 @@ def generate_crops(data_source, h, w): # TODO: refactor
     _gen_crops(DATA_dir, org_dir, h, w)
     _gen_crops(DATA_dir, mask_dir, h, w)
 
-def annotate_has_text(org_dir, mask_dir, h=None, w=None):
+def annotate_text_ox(data_source, h=None, w=None):
     ''' If h,w is passed, Use cropped directories. '''
     org_dir = Path(org_dir); mask_dir = Path(mask_dir)
     assert (h is None and w is None) or (h > 0 and w > 0)
@@ -30,29 +30,80 @@ def annotate_has_text(org_dir, mask_dir, h=None, w=None):
         mask_dir = mask_dir.parent / 'crop' / mask_stem
     assert org_dir.exists(), org_dir
     assert mask_dir.exists(), mask_dir
+    
+    threshold_8bit_rgb_mask = 333500
+    _annotate_text_ox_img_mask_lst(
+        org_dir, mask_dir, threshold_8bit_rgb_mask, h, w)
+    
+#---------------------------------------------------------------
+def _gen_crops(DATA_dir, img_dir, h, w, dst_crops_dir=None):
+    '''
+    DATA_dir: 데이터 소스의 DATA 디렉토리 경로
+    img_dir: 자르려는 이미지가 존재하는 디렉토리의 경로
+    h, w: 잘려져 나오는 crop의 크기
+    dst_crops_dir: crop이 저장되는 디렉토리 경로
+    '''
+    # Get src images
+    assert img_dir.exists()
+    
+    if dst_crops_dir is None:
+        dst_crops_dir = Path(
+            DATA_dir, 'crop', f'{img_dir.stem}.h{h}w{w}')
+    
+    # Generate (path, crop)s
+    paths = fu.human_sorted(fu.descendants(img_dir))
+    path_crop_seq = gen_crops.do(paths, h, w, dst_crops_dir)
+    
+    # look & feel check
+    '''
+    from funcy import take
+    for p, c in take(40, path_crop_seq):
+        print(' org:', p, c.shape);
+        cv2.imshow('c',c);
+        cv2.waitKey(0)
+    exit()
+    '''
+    
+    # Save to dst directories
+    os.makedirs(dst_crops_dir, exist_ok=True)
+    # TODO: makedirs parents.. preserve directory structure.
+    
+    print(f'start: _gen_crops({DATA_dir}, {img_dir}, {h}, {w}, {dst_crops_dir})')
+    for path, crop in path_crop_seq:
+        cv2.imwrite(path, cv2.cvtColor(crop, cv2.COLOR_RGB2BGR))
+    print('finished')
 
-    # Use only (h,w) sized crops
+def _annotate_text_ox(org_dir, mask_dir, threshold,
+                      h=None, w=None):
+    ''' If h,w is passed, Use only (h,w) sized crops. '''
     org_paths = fu.human_sorted(fu.descendants(org_dir))
     mask_paths = fu.human_sorted(fu.descendants(mask_dir))
     assert len(org_paths) == len(mask_paths)
+    
+    # Use only (h,w) sized crops.
     def is_hw_size(path):
-        img_w,img_h = imagesize.get(path)
-        return img_w == w and img_h == h
+        if h is not None and w is not None:
+            img_w,img_h = imagesize.get(path)
+            return img_w == w and img_h == h
+        else:
+            return True # Use all.
     org_crop_paths = fp.lfilter(is_hw_size, org_paths)
     mask_crop_paths = fp.lfilter(is_hw_size, mask_paths)
     #for o,m in zip(org_crop_paths, mask_crop_paths): print(o,m)
     
+    # Map mask_seq to has-text? (T/F)
     mask_seq = (cv2.cvtColor(cv2.imread(p), cv2.COLOR_BGR2RGB)
                 for p in mask_crop_paths) # TODO: rgba?
-    # Map mask_seq to has-text? (T/F)
     # Zip with org_crop_paths
     # Save to has_text.auto.1_{len(paths)}.yml
 
     # code for experiments. 
     #img_seq = (cv2.cvtColor(cv2.imread(p), cv2.COLOR_BGR2RGB) for p in org_crop_paths) # TODO: rgba?
-    # manual_has_text_annotation1000_from_m101_h256w256(img_seeq, mask_seq)
-
-def manual_has_text_annotation1000_from_m101_h256w256(iseq, mseq):
+    # __manual_has_text_annotation1000_from_m101_h256w256(img_seeq, mask_seq)
+        
+    
+#----------------------------------------------------------------
+def __manual_has_text_annotation1000_from_m101_h256w256(iseq, mseq):
     # 간단한 어노테이터 코드
     '''
     idxes = []
@@ -103,41 +154,3 @@ def manual_has_text_annotation1000_from_m101_h256w256(iseq, mseq):
     print(sorted(zip(thrs, ans_rates), key=lambda ta: ta[1])[-1])
     return no_text_idxes
     '''
-        
-#---------------------------------------------------------------
-def _gen_crops(DATA_dir, img_dir, h, w, dst_crops_dir=None):
-    '''
-    DATA_dir: 데이터 소스의 DATA 디렉토리 경로
-    img_dir: 자르려는 이미지가 존재하는 디렉토리의 경로
-    h, w: 잘려져 나오는 crop의 크기
-    dst_crops_dir: crop이 저장되는 디렉토리 경로
-    '''
-    # Get src images
-    assert img_dir.exists()
-    
-    if dst_crops_dir is None:
-        dst_crops_dir = Path(
-            DATA_dir, 'crop', f'{img_dir.stem}.h{h}w{w}')
-    
-    # Generate (path, crop)s
-    paths = fu.human_sorted(fu.descendants(img_dir))
-    path_crop_seq = gen_crops.do(paths, h, w, dst_crops_dir)
-    
-    # look & feel check
-    '''
-    from funcy import take
-    for p, c in take(40, path_crop_seq):
-        print(' org:', p, c.shape);
-        cv2.imshow('c',c);
-        cv2.waitKey(0)
-    exit()
-    '''
-    
-    # Save to dst directories
-    os.makedirs(dst_crops_dir, exist_ok=True)
-    # TODO: makedirs parents.. preserve directory structure.
-    
-    print(f'start: _gen_crops({DATA_dir}, {img_dir}, {h}, {w}, {dst_crops_dir})')
-    for path, crop in path_crop_seq:
-        cv2.imwrite(path, cv2.cvtColor(crop, cv2.COLOR_RGB2BGR))
-    print('finished')
