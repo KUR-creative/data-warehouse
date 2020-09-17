0b82f8cbff854a4537635c6cc7dd5835391884ab
kur <rhdnfka94@gmail.com>
2020-09-17 22:03:42 +0900

    Memoize core.crops.yxs

--- core/crops.py
+++ core/crops.py
@@ -2,23 +2,16 @@ from itertools import product
 
 from funcy import memoize
 
-from utils.etc_utils import factors, modulo_pad
+from utils.etc_utils import factorseq, modulo_pad
 
 
+@memoize
 def yxs(img_h, img_w, crop_h, crop_w):
     if crop_h > img_h or crop_w > img_w:
         raise ValueError(
             f'{crop_h} > {img_h} or {crop_w} > {img_w}')
-    padded_h = img_h + _modulo_pad(crop_h, img_h)
-    padded_w = img_w + _modulo_pad(crop_w, img_w)
-    hs = _factors(crop_h, padded_h)
-    ws = _factors(crop_w, padded_w)
+    padded_h = img_h + modulo_pad(crop_h, img_h)
+    padded_w = img_w + modulo_pad(crop_w, img_w)
+    hs = factorseq(crop_h, padded_h)
+    ws = factorseq(crop_w, padded_w)
     return list(product(hs, ws))
-
-@memoize
-def _modulo_pad(crop_size, img_size):
-    return modulo_pad(crop_size, img_size)
-
-@memoize
-def _factors(crop_size, img_size):
-    return factors(crop_size , img_size)
