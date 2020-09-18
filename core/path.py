30c0d6c1c028550182d4374455b16a1d81033987
kur <rhdnfka94@gmail.com>
2020-09-18 15:08:50 +0900

    Add core.path.dataset_root
    It can be more refactored, but I didn't.

--- core/path.py
+++ core/path.py
@@ -1,23 +1,36 @@
 from pathlib import Path
 
+#---------------------------------------------------------------
+def parentseq(path):
+    path = Path(path)
+    while path != Path('.'):
+        yield path
+        path = path.parent
 
+#---------------------------------------------------------------
 def is_data_source(path):
     return(Path(path, 'DATA').exists() and
            Path(path, 'META').exists() and
            Path(path, 'RELS').exists())
 
 def data_source(path):
-    ''' Get data-dource of path. It could be path itself. '''
-    def parents(path):
-        path = Path(path)
-        while path != Path('.'):
-            yield path
-            path = path.parent
-            
-    for parent in parents(path):
+    ''' Get data-source of path. It could be path itself. '''
+    for parent in parentseq(path):
         if is_data_source(parent):
             return str(parent)
     #return None
+    
+#---------------------------------------------------------------
+def is_dataset_root(path):
+    return(Path(path, 'DSET').exists() and
+           Path(path, 'META').exists() and
+           Path(path, 'OUTS').exists())
+
+def dataset_root(path):
+    ''' Get dataset-root of path. It could be path itself. '''
+    for parent in parentseq(path):
+        if is_dataset_root(parent):
+            return str(parent)
 
 '''
 from pprint import pprint
